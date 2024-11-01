b09b2a80fc3ae38f21bf42a0523098111a8183fa
Lizbeth Alpizar <lizbeth.alejandro@neoris.com>
2024-11-01 13:06:44 -0600

    first commit

---
+++ Photogram/Photogram/PhotogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotogramApp.swift
+//  Photogram
+//
+//  Created by lizbeth.alejandro on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
